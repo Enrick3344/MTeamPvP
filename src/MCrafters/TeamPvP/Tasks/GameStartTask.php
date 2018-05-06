38b551838f0ffc305f768af165ab9b83c9b24588
Enrick3344 <enrico3815@hotmail.fr>
2018-05-05 22:43:22 -0400

    Update GameStartTask.php

--- src/MCrafters/TeamPvP/Tasks/GameStartTask.php
+++ src/MCrafters/TeamPvP/Tasks/GameStartTask.php
@@ -24,7 +24,7 @@ class GameStartTask extends PluginTask
         $this->plugin = $plugin;
     }
 
-    public function onRun($tick)
+    public function onRun($currentTick)
     {
         $this->seconds -= 1;
 
